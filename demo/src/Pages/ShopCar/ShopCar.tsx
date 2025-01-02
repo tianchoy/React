02228d2efcd5b96a08b03433599b6157dfdc08d3
“tianchoy” <tianchoy@live.com>
2025-01-02 22:42:41 +0800

    change class

--- demo/src/Pages/ShopCar/ShopCar.tsx
+++ demo/src/Pages/ShopCar/ShopCar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import useCartStore from "../../Store/CarStore";
 import MyButton from "../../Components/MyButton";
 
@@ -10,56 +10,96 @@ interface Item {
   quantity: number; // 新增数量字段
 }
 
-const Cart: React.FC = () => {
-  const {
-    items,
-    addItem,
-    removeItem,
-    clearCart,
-    increaseQuantity,
-    decreaseQuantity,
-    totalPrice,
-  } = useCartStore();
+// 定义组件 Props 和 State 类型
+interface CartProps {}
 
-  const handleAddItem = () => {
+interface CartState {
+  items: Item[];
+  totalPrice: number;
+}
+
+class Cart extends Component<CartProps, CartState> {
+  // 订阅 Zustand Store 的状态
+  unsubscribe: () => void;
+
+  constructor(props: CartProps) {
+    super(props);
+    this.state = {
+      items: useCartStore.getState().items,
+      totalPrice: useCartStore.getState().totalPrice,
+    };
+
+    // 订阅 Zustand Store 的变化
+    this.unsubscribe = useCartStore.subscribe((state) => {
+      this.setState({
+        items: state.items,
+        totalPrice: state.totalPrice,
+      });
+    });
+  }
+
+  // 组件卸载时取消订阅
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  // 添加商品
+  handleAddItem = () => {
     const newItem: Item = {
       id: Math.random().toString(36).substr(2, 9), // 生成一个随机 ID
-      name: `Product ${items.length + 1}`,
+      name: `Product ${this.state.items.length + 1}`,
       price: Math.floor(Math.random() * 100) + 1, // 随机价格
       quantity: 1, // 默认数量为 1
     };
-    addItem(newItem);
+    useCartStore.getState().addItem(newItem);
   };
 
-  return (
-    <div>
-      <h1>Shopping Cart</h1>
-      <MyButton types="primary" BtnTtitle="Add Item" BtnClick={handleAddItem} />
-      <MyButton types="primary" BtnTtitle="Clear Cart" BtnClick={clearCart} />
-      <ul>
-        {items.map((item) => (
-          <li key={item.id}>
-            {item.name} - ${item.price} x {item.quantity}
-            <MyButton
-              types="primary"
-              BtnTtitle="+"
-              BtnClick={() => increaseQuantity(item.id)}
-            />
-            <MyButton
-              types="primary"
-              BtnTtitle="-"
-              BtnClick={() => decreaseQuantity(item.id)}
-            />
-            <MyButton
-              types="primary"
-              BtnTtitle="Remove"
-              BtnClick={() => removeItem(item.id)}
-            />
-          </li>
-        ))}
-      </ul>
-      <p>Total Price: ${totalPrice}</p>
-    </div>
-  );
-};
+  render() {
+    const { items, totalPrice } = this.state;
+
+    return (
+      <div>
+        <h1>Shopping Cart</h1>
+        <MyButton
+          types="primary"
+          BtnTtitle="Add Item"
+          BtnClick={this.handleAddItem}
+        />
+        <MyButton
+          types="primary"
+          BtnTtitle="Clear Cart"
+          BtnClick={useCartStore.getState().clearCart}
+        />
+        <ul>
+          {items.map((item) => (
+            <li key={item.id}>
+              {item.name} - ${item.price} x {item.quantity}
+              <MyButton
+                types="primary"
+                BtnTtitle="+"
+                BtnClick={() =>
+                  useCartStore.getState().increaseQuantity(item.id)
+                }
+              />
+              <MyButton
+                types="primary"
+                BtnTtitle="-"
+                BtnClick={() =>
+                  useCartStore.getState().decreaseQuantity(item.id)
+                }
+              />
+              <MyButton
+                types="primary"
+                BtnTtitle="Remove"
+                BtnClick={() => useCartStore.getState().removeItem(item.id)}
+              />
+            </li>
+          ))}
+        </ul>
+        <p>Total Price: ${totalPrice}</p>
+      </div>
+    );
+  }
+}
+
 export default Cart;
